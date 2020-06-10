98dbadf10772978b9f6eb454a3ab68cef3707600
MichaelGmx <474542562@qq.com>
2020-06-10 18:16:10 +0800

    加入dom event操作 示例

--- src/app/components/ng-example/dom-event/dom-event.component.ts
+++ src/app/components/ng-example/dom-event/dom-event.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { fromEvent, pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dom-event',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dom-event.component.scss']
 })
 export class DomEventComponent implements OnInit {
+  @ViewChild('eleObj', { static: true }) private eleObj: ElementRef;
+
+  private domObj: HTMLDivElement;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.domObj = this.eleObj.nativeElement;
+    fromEvent(this.domObj, 'touchend')
+      .subscribe(
+        (event: TouchEvent) => alert(event)
+      );
   }
 
 }
+
+
+// 可用事件名称有：click、mousedown、mouseup、mousemove、touchstart、touchend、touchmove
+// HTML DOM Event对象实例参考： https://www.w3school.com.cn/jsref/dom_obj_event.asp
