9cdc64eba5927bde5d06b716a588eda748b0703e
MichaelGmx <474542562@qq.com>
2020-12-01 11:51:51 +0800

    rxjs例子 修正

--- src/app/pages/rxjs/rxjs-example/rxjs-example.component.ts
+++ src/app/pages/rxjs/rxjs-example/rxjs-example.component.ts
@@ -20,14 +20,15 @@ export class RxjsExampleComponent implements OnInit {
   //  * 尽可能使用concat(rxjs)的静态方法
   //  * 虽然rxjs6.x 向下兼容 concat(rxjs/operators)的操作符 但将来会被移除
   //  */
-  // sourceOne$ = of(1, 2, 3);
+  // sourceOne$ = of(1, 2, 3).pipe(delay(3000));
   // sourceTwo$ = of(4, 5, 6);
   // // example$ = this.sourceOne$.pipe(concat(this.sourceTwo$));  // 此方式已被移除，虽然运行正常，但最好使用下面的方式
   // subscribe$ = concat(
-  //   this.sourceOne$.pipe(delay(1000)),
-  //   this.sourceTwo$.pipe(delay(2000)),
+  //   this.sourceOne$,
+  //   this.sourceTwo$,
   // ).subscribe(val => console.log('Example: Basic concat: ', val));
-  // // 【sourceTwo$会等待sourceOne$结束后，再执行，最终在3s后一同输出返回值】
+  // // 【sourceTwo$会等待3s 在sourceOne$结束后，再继续，最终在3s后一同输出返回值】
+  // // 【目的是：等待第一个完成后，再执行下一个】
   // // Example: Basic concat: 1
   // // Example: Basic concat: 2
   // // Example: Basic concat: 3
@@ -41,15 +42,15 @@ export class RxjsExampleComponent implements OnInit {
   //  * merge   静态方法
   //  * -- 同时进行，多个 observable 完成后一同发出订阅
   //  */
-  // first$ = of('1');
-  // second$ = of('2');
-  // thrid$ = of('3');
+  // first$ = of('1').pipe(delay(2000));
+  // second$ = of('2').pipe(delay(1000));
+  // thrid$ = of('3').pipe(delay(3000));
   // subscribe$ = merge(
-  //   this.first$.pipe(delay(2000)),
-  //   this.second$.pipe(delay(1000)),
-  //   this.thrid$.pipe(delay(3000)),
+  //   this.first$,
+  //   this.second$,
+  //   this.thrid$,
   // ).subscribe(res => console.log(res));
-  // // 【first$、second$、thrid$一同开始，最终在3s后一同输出返回值】
+  // // 【first$、second$、thrid$一同开始，最快的先输出】
   // // 2
   // // 1
   // // 3
@@ -62,14 +63,14 @@ export class RxjsExampleComponent implements OnInit {
   //  * 与concat相似，但zip会将 observables 合并为一个数组
   //  */
   // sourceOne$ = of('Hello');
-  // sourceTwo$ = of('World!');
-  // sourceThree$ = of('Goodbye');
-  // sourceFour$ = of('World!');
+  // sourceTwo$ = of('World!').pipe(delay(1000));
+  // sourceThree$ = of('Goodbye').pipe(delay(2000));
+  // sourceFour$ = of('World!').pipe(delay(3000));
   // subscribe$ = zip(
   //   this.sourceOne$,
-  //   this.sourceTwo$.pipe(delay(1000)),
-  //   this.sourceThree$.pipe(delay(2000)),
-  //   this.sourceFour$.pipe(delay(3000))
+  //   this.sourceTwo$,
+  //   this.sourceThree$,
+  //   this.sourceFour$
   // ).subscribe(val => console.log(val));
   // // ["Hello", "World!", "Goodbye", "World!"]
 
@@ -86,9 +87,9 @@ export class RxjsExampleComponent implements OnInit {
   // subscribe$ = this.example$.subscribe(val => {
   //   console.log(`With concatMap: ${val}`);
   // });
-  // // 【延迟2000ms的observable完成后，再订阅延迟1000ms的observable，最后一同输出】
-  // With concatMap: Delayed by: 2000ms
-  // With concatMap: Delayed by: 1000ms
+  // // 【延迟2000ms的observable完成后，再订阅延迟1000ms的observable。一共使用3000ms输出全部】
+  // // With concatMap: Delayed by: 2000ms
+  // // With concatMap: Delayed by: 1000ms
 
 
 
@@ -128,10 +129,10 @@ export class RxjsExampleComponent implements OnInit {
   //  * 综合示例：多个请求链式获取结果，最终获取最后一个的结果
   //  * 后一个的执行，需要得到前一个的结果后，才可以进行
   //  */
-  // first$ = of('isConnected');
-  // second$ = of({ address: 'DC:1D:30:9A:F9:7B', name: 'Printer_F97B' });
+  // first$ = of('isConnected').pipe(delay(1000));
+  // second$ = of({ address: 'DC:1D:30:9A:F9:7B', name: 'Printer_F97B' }).pipe(delay(2000));
   // // second$ = throwError(new Error('second error'));
-  // thrid$ = of('OK');
+  // thrid$ = of('OK').pipe(delay(1000));
 
   // subscribe = this.first$
   //   .pipe(
